6b9548c299e976547bb82eab3472af3ce3d5cd1f
Santiago Jose Castilla Ospina <126910931+SCO21@users.noreply.github.com>
2026-06-07 21:08:37 -0500

    lab07

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def pregunta_01():
     """
@@ -16,3 +19,97 @@ def pregunta_01():
     El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+    os.makedirs("files/plots", exist_ok=True)
+    plt.figure()
+
+    colors = {
+        "Television": "dimgray",
+        "Newspaper": "grey",
+        "Radio": "lightgrey",
+        "Internet": "tab:blue",
+    }
+
+    zorder = {
+        "Television": 1,
+        "Newspaper": 1,
+        "Radio": 1,
+        "Internet": 2,
+    }
+
+    linewidths = {
+        "Television": 2,
+        "Newspaper": 2,
+        "Radio": 2,
+        "Internet": 4,
+    }
+
+    df= pd.read_csv("files/input/news.csv", index_col=0)
+    for col in df.columns:
+        plt.plot(df[col], 
+                 color=colors[col],
+                 zorder=zorder[col],
+                 linewidth=linewidths[col],
+                 label=col)
+
+    plt.title("How people get their news", fontsize=16)
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["left"].set_visible(False)
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    for col in df.columns:
+        first_year = df.index[0]
+        plt.scatter(
+            x=first_year,
+            y=df[col][first_year],
+            color=colors[col],
+            zorder=zorder[col],
+        )
+
+        plt.text(
+            first_year - 0.2,
+            df[col][first_year],
+            col +" "+ str(df[col][first_year]) + "%",
+            ha="right",
+            va="center",
+            color=colors[col],
+        )
+
+        last_year = df.index[-1]
+        plt.scatter(
+            x=last_year,
+            y=df[col][last_year],
+            color=colors[col],
+            zorder=zorder[col],
+        )
+
+        plt.text(
+            last_year + 0.2,
+            df[col][last_year],
+            col +" "+ str(df[col][last_year]) + "%",
+            ha="left",
+            va="center",
+            color=colors[col],
+        )
+
+        last_year = df.index[-1]
+        plt.scatter(
+            x=last_year,
+            y=df[col][last_year],
+            color=colors[col],
+            zorder=zorder[col],
+        )
+
+    plt.xticks(
+        ticks=df.index,
+        labels=df.index,
+        ha="center",
+    )
+
+    plt.tight_layout()
+    plt.savefig("files/plots/news.png",)
+    plt.show()
+
+    return
+print(pregunta_01())
